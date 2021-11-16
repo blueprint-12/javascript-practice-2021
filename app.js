a2f605fead712311de0ffd9f07da81fa56114421
wonhee <86911858+blueprint-12@users.noreply.github.com>
2021-11-16 21:59:16 +0900

    create js,html,css files

---
+++ app.js
@@ -0,0 +1,13 @@
+const h1 = document.querySelector(".hello-one h1");
+
+function handleTitleClick() {
+    if(h1.style.color === "blue"){
+        h1.style.color = "tomato";
+    }else{
+        h1.style.color="blue";
+    }
+};
+
+
+h1.addEventListener("click", handleTitleClick);
+
