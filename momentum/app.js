5d823c06ca6b6a72f17609e9bc89aec094b307c7
wonhee <86911858+blueprint-12@users.noreply.github.com>
2021-11-24 22:05:56 +0900

    make a welcome message
    when user put her name on the blank input, the form tag disappears and the welcome message comes up with their user name.

--- momentum/app.js
+++ momentum/app.js
@@ -1,11 +1,18 @@
 
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
-event.preventDefault();
-console.log(event); 
+    event.preventDefault();
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    console.log(username);
+    greeting.innerText = "hello " + username;
+    greeting.innerText = `hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 };
-/*click event -> loginButton*/ 
 
 loginForm.addEventListener("submit", onLoginSubmit );
