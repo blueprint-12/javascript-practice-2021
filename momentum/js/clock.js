14a5b49fcff878ca8a74744c3a9f2a8853b56cc2
wonhee <86911858+blueprint-12@users.noreply.github.com>
2021-11-27 22:21:43 +0900

    Update clock.js
    by using padStart method we can display seconds in two letters.

--- momentum/js/clock.js
+++ momentum/js/clock.js
@@ -2,8 +2,12 @@ const clock = document.querySelector("h2#clock");
 
 function getClock(){
     const date = new Date();
-    clock.innerText = (`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 //1초의 대기 시간 후 에 internval 이 발생하기 때문에 한번 호출해 줘야 합니다.
 getClock();
 setInterval(getClock, 1000 );
+
