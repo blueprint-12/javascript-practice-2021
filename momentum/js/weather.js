51bcf7ff0d8947a0add0a4f13477cc9539bb77a6
wonhee <86911858+blueprint-12@users.noreply.github.com>
2021-12-03 21:43:06 +0900

    created weather info function
    by using weather API, we can have the info of user's location and it's weather

--- momentum/js/weather.js
+++ momentum/js/weather.js
@@ -1,5 +1,18 @@
+const API_KEY ="68de1d9c799906ce4441e0fb10644395";
+
 function onGeoOk(position){
-    console.log(position);
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+ // url 마지막에 &units=metric 을 추가하여 화씨->섭씨로 전환했습니다. 
+   const url  = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+   fetch(url).then(response => response.json()).then(data => {
+       const weather = document.querySelector("#weather span:first-child");
+       const city =document.querySelector("#weather span:last-child"); 
+       
+       city.innerText = data.name;
+       weather.innerText =  `${data.weather[0].main} / ${data.main.temp}`;
+    });
 };
 function onGeoError(){
     alert("can't find you, no weather for you");
